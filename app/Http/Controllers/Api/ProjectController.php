a3c838d5985d2ab3057c98766088ead6c0edb38e
JohnQuimson <john.quimson777@gmail.com>
2024-02-20 00:58:19 +0100

    feat: risposta backend ricerca

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -10,13 +10,33 @@ class ProjectController extends Controller
 {
     public function index()
     {
+        request()->validate([
+            'key' => ['nullable', 'string', 'min:3']
+        ]);
+        // $projects = Project::all();
 
-        $projects = Project::all();
-        // $projects = Project::paginate(10);
+
+        // select * from projects where title LIKE %esempio%;
+        if (request()->key) {
+            $projects = Project::where('title', 'LIKE', '%' . request()->key . '%')->paginate(9); //se non usi paginate, devi mettere ->get();
+        } else {
+            $projects = Project::paginate(9);
+        }
+
 
         return response()->json([
             'status' => true,
-            'data' => $projects
+            'results' => $projects
+        ]);
+    }
+
+    public function show(string $slug)
+    {
+        $project = Project::where('slug', $slug)->with('technologies', 'type')->first();
+
+        return response()->json([
+            'status' => true,
+            'result' => $project
         ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\ProjectController as AdminProjectController;
 use App\Http\Controllers\Api\ProjectController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -28,3 +29,6 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('projects', [ProjectController::class, 'index']);
+Route::get('projects/{slug}', [ProjectController::class, 'show']);
+
+// Route::resource('projects', AdminProjectController::class)->parameters(['projects' => 'project:slug']);
